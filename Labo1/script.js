b10a8b3b6bf6abc1d0e92258daf85b3dc4eb42c5
Johnny Da Costa <johnny.dacosta@he-arc.ch>
2017-03-10 20:31:46 +0100

    Amélioration de l'object FLOAT
    - ajout du tableau IEEEFormatBits
    - ajout de la méthode interchangFormat()
    - ajout  d'attributs pour mémoriser les datas

--- Labo1/script.js
+++ Labo1/script.js
@@ -1,29 +1,79 @@
 var float = {
-  x: 0, //nombre floatant
-  m: [], // tableau pour la mantisse
-  eBin: [], // tableau pour l'exposant
-  bits32: 23,
-  bits64: 52,
-  e: 127, //exposant (32bits, E=8 => 127 si n'est rien dit)
-  s: 0, //signe de bit (0->+ et 1->-)
   
+  /*************************************
+  Attributs de notre objet  
+  *************************************/
+  x: 0,       //nombre floatant
+  m: [],      // tableau pour la mantisse
+  mSize:23,
+  e: 0,       // exposant EMax
+  eSize:0,    // taille de l'exposant
+  eBin: [],   // tableau pour l'exposant
+  s: 0,       //signe de bits
+  nbBits: 32,  //nmombre de bit sur lequel on encode l'information
+ 
+  /*********************************************************************************
+    Selon la norme IEEE 754-1985
+    Voir tableau dans la sections Basic and interchange_formats
+    source : https://en.wikipedia.org/wiki/IEEE_floating_point#Interchange_formats
+  **********************************************************************************/ 
+  IEEEFormatBits: [
+    {bits: 16,  exponant: 5,  eMax: 15},
+    {bits: 32,  exponant: 8,  eMax: 127},
+    {bits: 64,  exponant: 11, eMax: 1023},
+    {bits: 128, exponant: 15, eMax: 16383},
+    {bits: 256, exponant: 19, eMax: 262143}
+  ],
   
-  //méthodes pour trouver le signe, mantisse et l'exposant
-  //permet de trouver le signe de bit
+  /**********************************************************************************
+   interchangeFormat() va nous permettre
+   de vérifié si le nombre de bits existe déjà selon la norme IEEE 754-1985
+   et de set la taille de l'exposant et son EMax
+  **********************************************************************************/
+  interchangeFormat : function(){
+    for (var i = 0; i < this.IEEEFormatBits.length; i++) {
+      if(this.IEEEFormatBits[i].bits == this.nbBits){
+        this.e = this.IEEEFormatBits[i].eMax;
+        this.eSize = this.IEEEFormatBits[i].exponant;
+        console.log("EMAX :"+this.e);
+        console.log("Exposant size :"+this.eSize);
+        return 0;
+      }
+    }
+    /*
+    calcule de la taille de l'exposant selon selon la norme IEEE 754-1985.
+    Ensuite enregistre la valeur de l'exposant Max et de sa taille dans le tableau
+    pour éviter de le recalculer si l'utilisateur nous le demande
+    */
+    this.eSize = Math.round(4 * Math.log(this.nbBits) / Math.log(2) - 13);
+    this.e = (Math.pow(2,this.eSize)/2)-1;
+    this.IEEEFormatBits.push({bits: this.nbBits, exponant: this.eSize, EMax: this.e });
+    return 0;
+  },
+  
+  setNbits : function(nbBits){
+    this.nbBits = Number(nbBits);
+    this.interchangeFormat();
+    this.mSize = this.nbBits - this.eSize - 1; // récupere la taille de la mantisse
+  },  
+  
+  /********************************************************
+  méthodes pour trouver le signe, mantisse et l'exposant
+  permet de trouver le signe de bit
+  *********************************************************/
   computeSigne : function(){
-     if(this.x < 0){ // si x est < 0 alors notre bits de signe devien -1
-       this.s=1
+     if(this.x < 0){
+       this.s=1;
        this.x *= -1;
      }else{
        this.s=0;
      }
   },
   
-
-  
-  //calcule l'exposant selon si l'utilisateur choisi 32 ou 64 bits
-  computeExposant : function(nbBits){
-    (nbBits===32) ? this.e = 127 : this.e = 1023;
+ /******************************************************
+  va calculer l'exposant en décimal
+ *******************************************************/
+  computeExposant : function(){
     if(this.x < 1){
       while (this.x<1){
         this.x*=2; 
@@ -42,22 +92,21 @@ var float = {
   
   convertExposantToBin : function(){
     this.eBin = this.e.toString(2).split('');
+    console.log("exposant dec to bin (taille "+ this.eSize+") -> "+ this.eBin);
   },
   
  makeArrayMantisseEmpty : function(){
    if(this.m.length > 0){this.m = [];}
  },
   
- computeMantisse : function(nbBits){
+ computeMantisse : function(){
     //on vérifie si le tableau m[] est vide ? 
     this.makeArrayMantisseEmpty(this.m);
     console.log("IN : computeMantisse");
     this.x--;
     console.log("x = "+this.x);
-    let sizeM;
-    (nbBits===32) ? this.sizeM = 23 : this.sizeM = 52;
-    console.log("Taille de la mantisse = "+this.sizeM);
-    for (var i = 0; i < this.sizeM; i++) {
+    console.log("Taille de la mantisse = "+this.mSize);
+    for (var i = 0; i < this.mSize; i++) {
         console.log("----------------------------------------");
         console.log("x = "+ this.x);
         this.x *= 2;
@@ -75,30 +124,29 @@ var float = {
     }
   },
   
-  casSpecialZero : function(n){
-    
+  casSpecialZero : function(){
     console.log("traiter le cas 0");
     this.makeArrayMantisseEmpty();
-    this.m = Array(n).fill(0);    //rempli le tableau de 0
+    this.m = Array(this.mSize).fill(0);    //rempli le tableau de 0
   },
   
   /*input*/
-  decTo: function(x, n){ //nombre de bits que l'on veut
+  decTo: function(x, nbBits){ //nombre de bits que l'on veut
     this.x = x;
+    this.setNbits(nbBits);
     if(this.x == 0 || this.x == -0){
-      this.casSpecialZero(n);
+      this.casSpecialZero();
       return 1;
     }
     this.computeSigne();
-    this.computeExposant(n);
-    this.computeMantisse(n);
+    this.computeExposant();
+    this.computeMantisse();
   },
   
   print: function(){
       $('bin').value = this.s+this.eBin.join('')+this.m.join('');
   } 
 };
-
 
 /*
 * fonction anonyme qui retourne l'élément qui porte l'id 
@@ -109,6 +157,6 @@ var $ = function(id){
 
 function decToBin(){
   var dec = $('dec');
-  float.decTo(dec.value, 32);
+  float.decTo(dec.value, 115);
   float.print();
 }; 

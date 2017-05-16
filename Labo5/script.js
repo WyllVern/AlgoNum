180c19ecc4be7d9698f8399e6d3f1630d65cb808
WyllVern <dany.chea@he-arc.ch>
2017-05-16 11:22:42 +0200

    Ui

--- Labo5/script.js
+++ Labo5/script.js
@@ -6,6 +6,9 @@ All rights reserved.
 
 function compute() {
   //Source: http://serge.mehl.free.fr/anx/meth_simpson.html
+  //Parce qu'avec la quadrature de Gauss, c'est compliqué.
+
+var $ = function( id ) { return document.getElementById( id ); };
   /*
   *a= borne inférieure
   *b= borne supérieure
@@ -36,6 +39,11 @@ function compute() {
   console.log((t1 - t0).toFixed(3) + " ms");
   console.log("pi=" + (4 * (j * h / 3)).toFixed(17));
   console.log("pi=" + Math.PI.toFixed(17));
+
+  $('piMachine').value="π= " + Math.PI.toFixed(17);
+  $('piSimpson').value="π= " + (4 * (j * h / 3)).toFixed(17);
+  $('time').value=(t1 - t0).toFixed(3) + " ms";
+
 
 }
 
